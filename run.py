14e5273b80c6aed5f2aabf8b62c0b522927af695
Michael O'Regan <w58fyus5@students.codeinstitute.net>
2025-01-01 16:29:46 +0000

    Convert string to date format using datetime

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import statistics
+from datetime import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,17 +14,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ter_data')
 
-def get_average_nav():
+def get_date_range():
     """
-    Get the average net asset value within the date range specified by the user
+    Get the date range specified by the user to build the report
     """
     print("Select date range for your TER:")
 
-    from_date_str = input("From Date:")
-    to_date_str = input("To Date:")
-    print(f"From Date is {from_date_str}")
-    print(f"To Date is {to_date_str}")
+    from_date_str = input("From Date (dd/mm/yyyy): ")
+    from_date_object = datetime.strptime(from_date_str, '%d/%m/%Y').date()
+
+    to_date_str = input("To Date (dd/mm/yyyy): ")
+    to_date_object = datetime.strptime(to_date_str, '%d/%m/%Y').date()
+    
+    print(f"From Date is {from_date_object}")
+    print(f"To Date is {to_date_object}")
+
+get_date_range()
+
+nav = SHEET.worksheet("net asset values")
+
+data = nav.get_all_values()
+
+print(data)
+
 
     
 
-get_average_nav()
